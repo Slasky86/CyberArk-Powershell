b0f065ce79546f973d4d05a0d1de4fe1724a5a32
Alexander Sageng <schumann_alexander@hotmail.com>
2024-02-06 15:37:29 +0100

    Update saferetentiondays based on list

---
+++ Safe Management/Update-DaysRetention.ps1
@@ -0,0 +1,59 @@
+﻿## Script to update safe retention setting for a number of days
+
+[CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        $PVWAURL,
+        [Parameter(Mandatory)]
+        $Safelist,
+        [Parameter()]
+        [string]$DaysOfRetention = "7"
+    )
+
+$Credential = Get-Credential
+
+try {
+
+    $Safes = Import-Csv $Safelist -Header "Safename" -Delimiter ";" -ErrorAction Stop
+
+}
+
+catch {
+
+    Write-Host -ForegroundColor Red "Something went wrong while importing the CSV file:"
+    $exception.message
+
+}
+
+$body = @{
+
+    "username"= $Credential.UserName;
+    "password"= ([System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Credential.Password)));
+    "concurrentSession"= "true"
+
+}
+
+$authentication = Invoke-RestMethod "$PVWAURL/PasswordVault/API/Auth/CyberArk/Logon" -Method Post -Body ($body | ConvertTo-Json) -ContentType application/json -UseBasicParsing
+
+$authbody = @{
+
+    "Authorization"="$authentication"
+
+}
+
+
+foreach ($Safe in $Safes.Safename) {
+
+    $searchbody = @{
+
+        "search"="$safe"
+
+    }
+
+    $Safedetails = Invoke-RestMethod "$PVWAURL/PasswordVault/API/Safes/" -Method Get -Headers $authbody -Body $searchbody | select -ExpandProperty Value
+
+    $safedetails.numberOfDaysRetention = $DaysOfRetention
+
+    Invoke-RestMethod "$PVWAURL/PasswordVault/API/Safes/$($Safedetails.safeUrlId)" -Method Put -Headers $authbody -Body ($Safedetails | ConvertTo-Json) -UseBasicParsing -ContentType application/json
+
+}
---
+++ Safe Management/Update-DaysRetention_psPAS.ps1
@@ -0,0 +1,48 @@
+﻿## Script to update safe retention setting for a number of days
+
+[CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        $PVWAURL,
+        [Parameter(Mandatory)]
+        $Safelist,
+        [Parameter()]
+        [string]$DaysOfRetention = "7"
+    )
+
+$Credential = Get-Credential
+
+try {
+
+    $Safes = Import-Csv $Safelist -Header "Safename" -Delimiter ";" -ErrorAction Stop
+
+}
+
+catch {
+
+    Write-Host -ForegroundColor Red "Something went wrong while importing the CSV file:"
+    $exception.message
+
+}
+
+try {
+
+    Import-Module psPAS
+
+}
+
+catch {
+
+    Write-Host -ForegroundColor Yellow "Powershell module psPAS not found, trying to install"
+    Install-Module -name psPAS -Force
+
+}
+
+New-PASSession -Credential $Credential -BaseURI $PVWAURL -concurrentSession $true
+
+
+foreach ($Safe in $Safes.Safename) {
+
+    Get-PASSafe -search $Safe | Set-PASSafe -NumberOfDaysRetention $DaysOfRetention
+
+}
